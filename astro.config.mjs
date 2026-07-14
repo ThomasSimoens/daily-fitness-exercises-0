9f3466b734463a0df500a6486467d7b93a678dfe
Thomas Simoens <thomas.simoens@gmail.com>
2026-07-14 22:04:04 +0200

    fix: static astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: "https://daily-fitness-exercises-0.pages.dev",
-  integrations: [tailwind(), cloudflare()],
+  integrations: [tailwind()],
+  output: "static",
 });
