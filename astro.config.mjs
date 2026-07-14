0221e8a7d311ddf181893392b0a9386c6885bd33
Thomas Simoens <thomas.simoens@gmail.com>
2026-07-14 20:57:15 +0200

    chore: improve styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: "https://daily-fitness-exercises-0.pages.dev",
+  integrations: [tailwind()],
 });
