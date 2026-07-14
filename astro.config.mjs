1a7f55ff23be1fe56e410c7d37d92d37dc440929
Thomas Simoens <thomas.simoens@gmail.com>
2026-07-14 21:54:57 +0200

    chore: add astro cloudflare setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: "https://daily-fitness-exercises-0.pages.dev",
-  integrations: [tailwind()],
+  integrations: [tailwind(), cloudflare()],
 });
